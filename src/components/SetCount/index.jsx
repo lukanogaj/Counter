eeaea6e29bcce09a64b2b6337f702808c1704676
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2023-12-01 13:09:12 +0000

    refactor

--- src/components/SetCount/index.jsx
+++ src/components/SetCount/index.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react';
 import styles from './index.module.scss';
 import SetDate from '../SetDate';
-// import SetWeek from '../SetWeek';
 
 const SetCount = () => {
   const [count, setCount] = useState(0);
   const [week, setWeek] = useState(0);
 
-  // Two functions for adding at removing weeks amount
-
   // Two functions to add and minus days
 
   const decrease = () => {
-    setCount((count) => count - 1);
+    count > 0 && setCount((count) => count - 1);
     count >= 0 && count % 7 === 1 && setWeek((week) => week - 1);
   };
 
@@ -23,7 +20,6 @@ const SetCount = () => {
   };
 
   console.log(count % 7);
-  // console.log(step);
   return (
     <div className={styles.container}>
       <div className={styles.weeks}>
@@ -31,10 +27,10 @@ const SetCount = () => {
       </div>
       <div className={styles.buttons}>
         <button
+          className={styles.btn}
           onClick={() => {
             decrease();
           }}
-          className={styles.btn}
         >
           -
         </button>
